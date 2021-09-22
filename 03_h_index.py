f1201f3bbad6c9e60355ae3d35aaf8aa33c5bede
Velbl Stefan <velblstefan@gmail.com>
2021-09-22 22:55:59 +0200

    03 - H-index.

--- 03_h_index.py
+++ 03_h_index.py
@@ -1,28 +1,25 @@
 # array of publications with different number of citations
-publications = [5, 3, 3, 1, 0]
-
-# get max value from an array
-def get_max_value(array):
-    max_el = array[0]
-    for element in range(0, len(array) - 1):
-        next_element = element + 1
-        if array[element] >= max_el:
-            max_el = array[element]
-    return max_el
+publications = [100, 5, 4, 7, 5]
 
 
 def h_index(publications):
-    num_of_citations = 0
-    index = 0
-    max_num_of_citations = get_max_value(publications)
+    n = len(publications)
+    citations = [0] * (n + 1)
 
-    for num_of_publications in range(1, max_num_of_citations):
-        if num_of_citations == num_of_publications:
-            return num_of_citations
+    for pub in publications:
+        if pub < n:
+            citations[pub] += 1
         else:
-            num_of_citations = publications[index]
-            index += 1
-    return None
+            citations[n] += 1
+    print(citations)
+    total = 0
+    i = n
+    while i >= 0:
+        total += citations[i]
+        if total >= i:
+            return i
+        i -= 1
+    return i
 
 
 print(h_index(publications))
